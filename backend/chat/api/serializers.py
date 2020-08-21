f1e2ad55df048b51273ffbd4344db937b154052d
dmitriy <dmitriyvek@mail.ru>
2020-08-21 12:26:54 +0300

    add last_message to chat serializer

--- backend/chat/api/serializers.py
+++ backend/chat/api/serializers.py
@@ -1,29 +1,22 @@
 from rest_framework import serializers
 
-from chat.models import Chat, Profile
-# from chat.services import get_profile_by_username
+from chat.models import Chat, Message
 
 
-class ProfileSerializer(serializers.StringRelatedField):
-    def to_internal_value(self, value):
-        return value
+class MessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        exclude = ('id',)
 
 
 class ChatSerializer(serializers.ModelSerializer):
-    participant_list = ProfileSerializer(many=True)
+    last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Chat
-        fields = ('id', 'participant_list')
+        fields = ('id', 'participant_list', 'last_message')
         read_only = ('id',)
 
-    # def create(self, validated_data):
-    #     # print(validated_data)
-    #     participant_list = validated_data.pop('participant_list')
-    #     chat = Chat()
-    #     chat.save()
-    #     for username in participant_list:
-    #         profile = get_profile_by_username(username)
-    #         chat.participant_list.add(profile)
-    #     chat.save()
-    #     return chat
+    def get_last_message(self, obj):
+        return MessageSerializer(obj.message_list.last()).data
