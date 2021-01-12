6024635aff44cec9549ae90b08eba7e087d85f49
dmitriyvek <www@dmitriyvek.com>
2021-01-12 14:59:41 +0000

    fix .env

--- backend/bin/start_daphne.sh
+++ backend/bin/start_daphne.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/dmitriy/1programming/new_chat_app/backend/env/bin/activate
-source  /home/dmitriy/1programming/new_chat_app/.env
-# python /home/dmitriy/1programming/new_chat_app/backend/manage.py migrate
-exec daphne -b $HOST -p $DAPHNE_PORT config.asgi:application
+source /home/www/code/chat/backend/env/bin/activate
+source  /home/www/code/chat/.env
+# python /home/www/code/backend/manage.py migrate
+exec daphne -b $HOST_IP -p $DAPHNE_PORT config.asgi:application
--- backend/bin/start_gunicorn.sh
+++ backend/bin/start_gunicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/dmitriy/1programming/new_chat_app/backend/env/bin/activate
-source  /home/dmitriy/1programming/new_chat_app/.env
-python /home/dmitriy/1programming/new_chat_app/backend/manage.py migrate
-exec gunicorn -c "/home/dmitriy/1programming/new_chat_app/backend/config/gunicorn_config.py" config.wsgi:application
+source /home/www/code/chat/backend/env/bin/activate
+source  /home/www/code/chat/.env
+python /home/www/code/chat/backend/manage.py migrate
+exec gunicorn -c "/home/www/code/chat/backend/config/gunicorn_config.py" config.wsgi:application
