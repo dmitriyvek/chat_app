f141d2ea1c803c18df8ccdde367a9aeb271d7ed5
dmitriy <dmitriyvek@mail.ru>
2021-01-14 13:21:51 +0300

    add default static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 # STATICFILES_DIRS = [
 #     Path(BASE_DIR, 'static'),
 # ]
